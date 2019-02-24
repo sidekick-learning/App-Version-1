d33f33a08c8ff7e2fe950861e0dde98d4e98c6cd
avinash <avinashuddaraju@gmail.com>
2019-02-24 06:34:01 -0700

    no idea what they are

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator, createAppContainer, createSwitchNavigator, createDrawerNavigator } from 'react-navigation';
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from '@expo/vector-icons/Ionicons';
+
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';

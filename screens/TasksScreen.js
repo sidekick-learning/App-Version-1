67b0120946dd7f185734e05f7b68a17267554800
myannutz <35411438+myannutz@users.noreply.github.com>
2019-02-24 02:13:47 -0700

    I got a checkbox to work on the second tab, the TasksScreen tab

--- screens/TasksScreen.js
+++ screens/TasksScreen.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  CheckBox,
+  View,
+  Platform,
+  Alert
+} from 'react-native';
 
 export default class TasksScreen extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      check:false
+  }
+}
   static navigationOptions = {
     title: 'Tasks',
   };
 
+
+
   render() {
     return (
-        <Text style={styles.container}>
-          text
-        </Text>
-
-
+      <View style={{flexDirection: 'row'}}>
+          <CheckBox
+            value={this.state.checked}
+            onValueChange={() => this.setState({ checked: !this.state.checked })}
+          />
+          <Text style={{marginTop: 5}}>this is a checkbox</Text>
+        </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: 'white',
-  },
-});

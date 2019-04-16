f0bb7e73ae7742eb0003f27df9d5998e1102dd92
Dustin Ramsay <dustin@ucar.edu>
2019-04-16 09:24:02 -0600

    update 4/16

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, TabNavigator, createBottomTabNavigator, createAppContainer, createSwitchNavigator, createDrawerNavigator } from 'react-navigation';
+import Icon from '@expo/vector-icons/Ionicons';
+
+//import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+//import ProgressScreen from '../screens/ProgressScreen';
+//import PrioritizerScreen from '../screens/PrioritizerScreen';
+
+export const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Tasks',
+  TabBarIcon: ({ focused }) => (
+    <tabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios' ? 'ios-calendar': 'md-calendar'
+      }
+    />
+  ),
+};
+
+/*
+const ProgressStack = createStackNavigator({
+  Links: ProgressScreen,
+});
+
+ProgressStack.navigationOptions = {
+  tabBarLabel: 'Progress',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-clipboard' : 'md-clipboard'}
+    />
+  ),
+};
+
+const PrioritizerStack = createStackNavigator({
+  Settings: PrioritizerScreen,
+});
+
+PrioritizerStack.navigationOptions = {
+  tabBarLabel: 'prioritizer',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-list-box' : 'md-list-box'}
+    />
+  ),
+};
+*/
+
+export default createBottomTabNavigator({
+  HomeStack,
+  //ProgressStack,
+  //PrioritizerStack,
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import {
+  TextInput,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import Note from '../components/Note';
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Tasks',
+  };
+
+  constructor(props){
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: '',
+      noteText2: '',
+    }
+  }
+
+  render() {
+    let notes = this.state.noteArray.map((val,key)=>{
+      return <Note key={key} keyval={key} val={val}
+              deleteMethod={()=> this.deleteNote(key)} />
+    });
+    return (
+      <View style={styles.container}>
+      <ScrollView style = {styles.scrollcontainer}>
+      <TextInput
+      style={styles.textInput}
+      onChangeText={(noteText)=> this.setState({noteText})}
+      value={this.state.noteText}
+      placeholder='Write a Task to add'
+      placeholderTextColor='white'
+      underlineColorAndroid='transparent'>
+      </TextInput>
+      {notes}
+      </ScrollView>
+      <View style={styles.footer}>
+      </View>
+      <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
+       <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
+       <Text style={styles.addButtonText}>+</Text>
+      </TouchableOpacity>
+      </View>
+    );
+  }
+
+  /*getDataJSON(){
+    var assignment = 
+  }*/
+
+  addNote(){
+    this.state.noteArray.push({'note': this.state.noteText})
+    this.setState({noteArray: this.state.noteArray})
+    this.setState({noteText: this.placeholder});
+  }
+
+  deleteNote(key){
+    this.state.noteArray.splice(key,1);
+    this.setState({noteArray: this.state.noteArray})
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  scrollcontainer: {
+    flex:1,
+    marginBottom:100,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+  },
+  textInput:{
+      alignSelf: 'stretch',
+      color: '#fff',
+      padding: 20,
+      backgroundColor: '#252525',
+      borderTopWidth: 2,
+      borderTopColor: '#ededed',
+    },
+  addButton:{
+    marginBottom:25,
+    paddingTop:5,
+    paddingBottom:15,
+    marginLeft:15,
+    marginRight:15,
+    backgroundColor: '#ff7034',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  addButtonText: {
+    color:'#fff',
+    fontSize: 24,
+  },
+});
